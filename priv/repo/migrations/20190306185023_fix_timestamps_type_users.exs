f068c984ad12ce42e230e6e047e83eef0836afab
Alberto Seoane <33812609+alchexmist@users.noreply.github.com>
2019-03-06 20:46:54 +0100

    Eliminados comandos innecesarios de migraciones.

--- priv/repo/migrations/20190306185023_fix_timestamps_type_users.exs
+++ priv/repo/migrations/20190306185023_fix_timestamps_type_users.exs
@@ -2,8 +2,8 @@ defmodule Erlnote.Repo.Migrations.FixTimestampsTypeUsers do
   use Ecto.Migration
 
   def change do
-    execute "ALTER TABLE users ALTER COLUMN inserted_at DROP NOT NULL"
-    execute "ALTER TABLE users ALTER COLUMN updated_at DROP NOT NULL"
+    # execute "ALTER TABLE users ALTER COLUMN inserted_at DROP NOT NULL"
+    # execute "ALTER TABLE users ALTER COLUMN updated_at DROP NOT NULL"
     alter table(:users) do
       remove :inserted_at
       remove :updated_at
--- priv/repo/migrations/20190306191610_fix_timestamps_type_all.exs
+++ priv/repo/migrations/20190306191610_fix_timestamps_type_all.exs
@@ -2,8 +2,8 @@ defmodule Erlnote.Repo.Migrations.FixTimestampsTypeAll do
   use Ecto.Migration
 
   def change do
-    execute "ALTER TABLE boards_users ALTER COLUMN inserted_at DROP NOT NULL"
-    execute "ALTER TABLE boards_users ALTER COLUMN updated_at DROP NOT NULL"
+    # execute "ALTER TABLE boards_users ALTER COLUMN inserted_at DROP NOT NULL"
+    # execute "ALTER TABLE boards_users ALTER COLUMN updated_at DROP NOT NULL"
     alter table(:boards_users) do
       remove :inserted_at
       remove :updated_at
@@ -11,8 +11,8 @@ defmodule Erlnote.Repo.Migrations.FixTimestampsTypeAll do
       timestamps(type: :utc_datetime)
     end
 
-    execute "ALTER TABLE boards ALTER COLUMN inserted_at DROP NOT NULL"
-    execute "ALTER TABLE boards ALTER COLUMN updated_at DROP NOT NULL"
+    # execute "ALTER TABLE boards ALTER COLUMN inserted_at DROP NOT NULL"
+    # execute "ALTER TABLE boards ALTER COLUMN updated_at DROP NOT NULL"
     alter table(:boards) do
       remove :inserted_at
       remove :updated_at
@@ -20,8 +20,8 @@ defmodule Erlnote.Repo.Migrations.FixTimestampsTypeAll do
       timestamps(type: :utc_datetime)
     end
 
-    execute "ALTER TABLE notes_tags ALTER COLUMN inserted_at DROP NOT NULL"
-    execute "ALTER TABLE notes_tags ALTER COLUMN updated_at DROP NOT NULL"
+    # execute "ALTER TABLE notes_tags ALTER COLUMN inserted_at DROP NOT NULL"
+    # execute "ALTER TABLE notes_tags ALTER COLUMN updated_at DROP NOT NULL"
     alter table(:notes_tags) do
       remove :inserted_at
       remove :updated_at
@@ -29,8 +29,8 @@ defmodule Erlnote.Repo.Migrations.FixTimestampsTypeAll do
       timestamps(type: :utc_datetime)
     end
 
-    execute "ALTER TABLE notes_users ALTER COLUMN inserted_at DROP NOT NULL"
-    execute "ALTER TABLE notes_users ALTER COLUMN updated_at DROP NOT NULL"
+    # execute "ALTER TABLE notes_users ALTER COLUMN inserted_at DROP NOT NULL"
+    # execute "ALTER TABLE notes_users ALTER COLUMN updated_at DROP NOT NULL"
     alter table(:notes_users) do
       remove :inserted_at
       remove :updated_at
@@ -38,8 +38,8 @@ defmodule Erlnote.Repo.Migrations.FixTimestampsTypeAll do
       timestamps(type: :utc_datetime)
     end
 
-    execute "ALTER TABLE notes ALTER COLUMN inserted_at DROP NOT NULL"
-    execute "ALTER TABLE notes ALTER COLUMN updated_at DROP NOT NULL"
+    # execute "ALTER TABLE notes ALTER COLUMN inserted_at DROP NOT NULL"
+    # execute "ALTER TABLE notes ALTER COLUMN updated_at DROP NOT NULL"
     alter table(:notes) do
       remove :inserted_at
       remove :updated_at
@@ -47,8 +47,8 @@ defmodule Erlnote.Repo.Migrations.FixTimestampsTypeAll do
       timestamps(type: :utc_datetime)
     end
 
-    execute "ALTER TABLE notepads_tags ALTER COLUMN inserted_at DROP NOT NULL"
-    execute "ALTER TABLE notepads_tags ALTER COLUMN updated_at DROP NOT NULL"
+    # execute "ALTER TABLE notepads_tags ALTER COLUMN inserted_at DROP NOT NULL"
+    # execute "ALTER TABLE notepads_tags ALTER COLUMN updated_at DROP NOT NULL"
     alter table(:notepads_tags) do
       remove :inserted_at
       remove :updated_at
@@ -56,8 +56,8 @@ defmodule Erlnote.Repo.Migrations.FixTimestampsTypeAll do
       timestamps(type: :utc_datetime)
     end
 
-    execute "ALTER TABLE notepads ALTER COLUMN inserted_at DROP NOT NULL"
-    execute "ALTER TABLE notepads ALTER COLUMN updated_at DROP NOT NULL"
+    # execute "ALTER TABLE notepads ALTER COLUMN inserted_at DROP NOT NULL"
+    # execute "ALTER TABLE notepads ALTER COLUMN updated_at DROP NOT NULL"
     alter table(:notepads) do
       remove :inserted_at
       remove :updated_at
@@ -65,8 +65,8 @@ defmodule Erlnote.Repo.Migrations.FixTimestampsTypeAll do
       timestamps(type: :utc_datetime)
     end
 
-    execute "ALTER TABLE tags ALTER COLUMN inserted_at DROP NOT NULL"
-    execute "ALTER TABLE tags ALTER COLUMN updated_at DROP NOT NULL"
+    # execute "ALTER TABLE tags ALTER COLUMN inserted_at DROP NOT NULL"
+    # execute "ALTER TABLE tags ALTER COLUMN updated_at DROP NOT NULL"
     alter table(:tags) do
       remove :inserted_at
       remove :updated_at
