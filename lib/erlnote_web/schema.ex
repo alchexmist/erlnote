8a034de0e09dbecd31da7d7c0d1e6c15badfcb3b
Alberto Seoane <33812609+alchexmist@users.noreply.github.com>
2019-05-08 19:54:12 +0200

    Añadida consulta GraphQL para obtener los usuarios definidos en el sistema. {   users {     id     name     username   } }
    {
      "data": {
        "users": [
          {
            "username": "asm",
            "name": "asm",
            "id": "1"
          },
          {
            "username": "jsg",
            "name": "jsg",
            "id": "2"
          },
          {
            "username": "mnmc",
            "name": "mnmc",
            "id": "3"
          }
        ]
      }
    }

--- lib/erlnote_web/schema.ex
+++ lib/erlnote_web/schema.ex
@@ -1,9 +1,18 @@
 defmodule ErlnoteWeb.Schema do
     use Absinthe.Schema
 
-    query do
-        # Other query fields
+    alias Erlnote.{Repo, Accounts}
 
+    query do
+      @desc "The list of available users in the system"
+      field :users, list_of(:user) do
+        resolve fn _, _, _ -> {:ok, Accounts.list_users} end
+      end
     end
 
+    object :user do
+      field :id, :id
+      field :name, :string
+      field :username, :string  
+    end
 end
