eb8e7cf44e7113ed39767a4176fc27e6f3059d67
Alberto Seoane <33812609+alchexmist@users.noreply.github.com>
2019-05-09 20:16:52 +0200

    Añadida breve documentación a la prueba.

--- test/erlnote_web/schema/query/users_test.exs
+++ test/erlnote_web/schema/query/users_test.exs
@@ -1,4 +1,12 @@
 defmodule ErlnoteWeb.Schema.Query.UsersTest do
+  # ErlnoteWeb.Schema.Query.UsersTest sets async: true which means that this test case
+  # will be run in parallel with other test cases. While individual tests
+  # within the case still run serially, this can greatly increase overall
+  # test speeds. It is possible to encounter strange behavior with
+  # asynchronous tests, but thanks to the Ecto.Adapters.SQL.Sandbox, async
+  # tests involving a database can be done without worry. This means that
+  # the vast majority of tests in your Phoenix application will be able to
+  # be run asynchronously.
   use ErlnoteWeb.ConnCase, async: true
 
   setup do
