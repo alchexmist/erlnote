654548440435d2560ead1bdbe43bfbc1abef3c4c
Alberto Seoane <33812609+alchexmist@users.noreply.github.com>
2019-09-06 23:41:24 +0200

    Corregido error crítico en migración por tabla duplicada.

--- priv/repo/migrations/20190305182118_fix_notepads_tags.exs
+++ priv/repo/migrations/20190305182118_fix_notepads_tags.exs
@@ -2,11 +2,13 @@ defmodule Erlnote.Repo.Migrations.FixNotepadsTags do
   use Ecto.Migration
 
   def change do
+    drop index(:notepads_tags, [:notepad_id])
     execute "ALTER TABLE notepads_tags DROP CONSTRAINT notepads_tags_notepad_id_fkey"
     execute "ALTER TABLE notepads_tags DROP CONSTRAINT notepads_tags_tag_id_fkey"
     alter table(:notepads_tags, primary_key: false) do
       modify :notepad_id, references(:notepads, on_delete: :delete_all), null: false
       modify :tag_id, references(:tags, on_delete: :delete_all), null: false
     end
+    create index(:notepads_tags, [:notepad_id])
   end
 end
